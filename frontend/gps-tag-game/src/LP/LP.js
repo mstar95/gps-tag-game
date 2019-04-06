b9cce4f7479c6785584fecfa3284e1256f79d94d
mstar95 <m.star@wp.pl>
2019-04-06 15:47:48 +0200

    map poc

---
+++ frontend/gps-tag-game/src/LP/LP.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const LP = () => (<div>HELLO WORD!</div>)
+
+export default LP
---
+++ frontend/gps-tag-game/src/rooms/roomsComponent.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const RoomsComponent = () => (<div>HELLO WORD!</div>)
+
+export default RoomsComponent
