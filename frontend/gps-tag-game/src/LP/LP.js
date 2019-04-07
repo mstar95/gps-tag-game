0e9395b77b3ae4e8d71c6cc86fdbbdfb1d27e3f6
mstar95 <m.star@wp.pl>
2019-04-07 16:54:34 +0200

    xd

--- frontend/gps-tag-game/src/LP/LP.js
+++ frontend/gps-tag-game/src/LP/LP.js
@@ -1,12 +1,19 @@
 import React from "react";
-import img from "../lp.jpg"
+import "./login.css"
 
 const LP = () => {
     return (
-        <div className="lpImg-container">
-            <img className="lpImg" src={img}/>
+        <div className="box">
+            <h2>Login</h2>
+            <form>
+                <div className="inputBox">
+                    <input name="email" required onKeyUp="this.setAttribute('value', this.value);" value=""/>
+                        <label>Nazwa</label>
+                </div>
+                <input type="submit" name="Do" value="Dołącz"/>
+            </form>
         </div>
-    )
+)
 };
 
 
